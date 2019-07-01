181b8a9bb3f3d4947cfd8c358ab944e84c77c764
Morris <mburumorris@gmail.com>
2019-07-01 14:09:01 +0200

    pagination

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -8,17 +8,22 @@ import ErrorMessage from '../Error';
 
 
 const GET_REPOSITORIES_OF_CURRENT_USER = gql`
-	{
+	query($cursor: String){
 		viewer {
 			repositories(
 				first: 5
 				orderBy: {direction: DESC, field: STARGAZERS}
+				after: $cursor
 			){
 				edges {
 					node {
 						...repository
 					}
 
+				}
+				pageInfo {
+					endCursor
+					hasNextPage
 				}
 			}
 
@@ -28,8 +33,11 @@ const GET_REPOSITORIES_OF_CURRENT_USER = gql`
 `;
 
 const Profile = () => ( 
-	<Query query={GET_REPOSITORIES_OF_CURRENT_USER}>
-		{({data, loading, error }) =>{
+	<Query 
+		query={GET_REPOSITORIES_OF_CURRENT_USER}
+		notifyOnNetworkStatusChange={true}
+	>
+		{({data, loading, error, fetchMore }) =>{
 			if(error){
 				return <ErrorMessage error={error}/>
 			}
@@ -39,7 +47,13 @@ const Profile = () => (
 				return <Loading/>;
 			}
 
-			return <RepositoryList repositories={viewer.repositories}/>;
+			return (
+				<RepositoryList 
+					loading={loading}
+					repositories={viewer.repositories}
+					fetchMore={fetchMore}
+				/>
+			);
 		}} 
 	</Query> 
 );
--- src/Repository/RepositoryList/index.js
+++ src/Repository/RepositoryList/index.js
@@ -1,16 +1,70 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
+import FetchMore from '../../FetchMore';
 import RepositoryItem from '../RepositoryItem';
+import Loading from '../../Loading';
 
 import '../style.css';
 
+const updateQuery = (previousResult, {fetchMoreResult}) => {
+	if(!fetchMoreResult){
+		return previousResult;
+	}
 
-const RepositoryList = ({ repositories}) => 
-	repositories.edges.map(({node}) => (
-		<div key={node.id} className="RepositoryItem">
-			<RepositoryItem{...node}/>
-		</div>
-	));
+	return {
+		...previousResult,
+		viewer: {
+			...previousResult.viewer,
+			repositories: {
+				...previousResult.viewer.repositories,
+				...fetchMoreResult.viewer.repositories,
+				edges: [
+					...previousResult.viewer.repositories.edges,
+					...fetchMoreResult.viewer.Repositories.edges,
+				],
+			},
+		},
+	};
+};
+const RepositoryList = ({ repositories,loading, fetchMore }) => (
+	<Fragment>
+		{repositories.edges.map(({node}) => (
+			<div key={node.id} className="RepositoryItem">
+				<RepositoryItem{...node}/>
+			</div>
+		))}
+		
+		<FetchMore
+			loading={loading}
+			hasNextPage={repositories.pageInfo.hasNextPage}
+			variables={{
+				cursor: repositories.pageInfo.endCursor,
+			}}
+			updateQuery={updateQuery}
+			fetchMore={fetchMore}
+		>
+			Repositories
+		</FetchMore>
+		{loading ? (
+			<Loading/>
+		) : (
+			repositories.pageInfo.hasNextPage && (
+			<button
+				type="button"
+				onClick={() =>
+					fetchMore({
+						variables: {
+							cursor: repositories.pageInfo.endCursor,
+						}, 
+					})
+				}
+			>
+				More Repositories
+			</button>
+			)
+		)}
 
+	</Fragment>
+);
 
 export default RepositoryList;
